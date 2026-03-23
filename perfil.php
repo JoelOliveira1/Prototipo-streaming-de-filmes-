d5cc289aa32b269fc3e0af200ea095a252e83099
joel <oliveira.wjc@gmail.com>
2026-03-23 11:07:57 -0300

    adicionando o logout

--- perfil.php
+++ perfil.php
@@ -30,6 +30,7 @@ $usuario = $result->fetch_assoc();
 <a href="#">Séries</a>
 <a href="#">Minha Lista</a>
 <a href="perfil.php">Perfil</a>
+<a href="logout.php"class= "Logout">Sair</a>
 </nav>
 
 </header>
