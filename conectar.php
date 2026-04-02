b7a0018c0e76b560c9ac3c1ebac25f4f5b0ab6e2
Davi Maltez <maltezdavid438@gmail.com>
2026-04-01 21:10:43 -0300

    Update conectar.php

--- conectar.php
+++ conectar.php
@@ -1,18 +1,14 @@
 <?php
-
-$host     = "localhost";
-$usuario  = "root";       
-$senha    = "";           
-$banco    = "nityfrix";   
-
+$host    = "sql306.infinityfree.com";     // Database Hostname do painel
+$usuario = "if0_41557948";               // Username da conta
+$senha   = "Dick5920";         // Senha da conta de hospedagem
+$banco   = "if0_41557948_nityfrix";      // Nome completo do banco, com prefixo
 
 $conn = new mysqli($host, $usuario, $senha, $banco);
 
-// Verifica se houve erro na conexão
 if ($conn->connect_error) {
     die("Erro ao conectar ao banco de dados: " . $conn->connect_error);
 }
-
 
 $conn->set_charset("utf8");
 ?>
